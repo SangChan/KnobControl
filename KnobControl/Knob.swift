2b036ebf2dcce5c6fcac66686ea2880467627ad9
SangChan Lee <sangchan82@gmail.com>
2018-08-04 12:09:42 +0900

    add public procedure on some of it

--- KnobControl/Knob.swift
+++ KnobControl/Knob.swift
@@ -27,17 +27,16 @@
 /// THE SOFTWARE.
 
 import UIKit
-import UIKit.UIGestureRecognizerSubclass
 
 @IBDesignable public class Knob: UIControl {
     var minimumValue: Float = 0
     var maximumValue: Float = 1
     var isContinuous = true
     
-    private (set) var value: Float = 0
+    public var value: Float = 0
     private let renderer = KnobRenderer()
     
-    @IBInspectable var lineWidth: CGFloat {
+    @IBInspectable public var lineWidth: CGFloat {
         get { return renderer.lineWidth }
         set { renderer.lineWidth = newValue }
     }
@@ -52,7 +51,7 @@ import UIKit.UIGestureRecognizerSubclass
         set { renderer.endAngle = newValue }
     }
     
-    @IBInspectable var pointerLength: CGFloat {
+    @IBInspectable public var pointerLength: CGFloat {
         get { return renderer.pointerLength }
         set { renderer.pointerLength = newValue }
     }
@@ -110,7 +109,7 @@ import UIKit.UIGestureRecognizerSubclass
         }
     }
     
-    func setValue(_ newValue: Float, animated: Bool = false) {
+    public func setValue(_ newValue: Float, animated: Bool = false) {
         value = min(maximumValue, max(minimumValue, newValue))
         
         let angleRange = endAngle - startAngle
@@ -125,142 +124,6 @@ extension Knob {
         super.prepareForInterfaceBuilder()
         
         renderer.updateBounds(bounds)
-    }
-}
-
-private class KnobRenderer {
-    var color: UIColor = .blue {
-        didSet {
-            trackLayer.strokeColor = color.cgColor
-            pointerLayer.strokeColor = color.cgColor
-        }
-    }
-    
-    var lineWidth: CGFloat = 2 {
-        didSet {
-            trackLayer.lineWidth = lineWidth
-            pointerLayer.lineWidth = lineWidth
-            updateTrackLayerPath()
-            updatePointerLayerPath()
-        }
-    }
-    
-    var startAngle: CGFloat = CGFloat(-Double.pi) * 11 / 8 {
-        didSet {
-            updateTrackLayerPath()
-        }
-    }
-    
-    var endAngle: CGFloat = CGFloat(Double.pi) * 3 / 8 {
-        didSet {
-            updateTrackLayerPath()
-        }
-    }
-    
-    var pointerLength: CGFloat = 6 {
-        didSet {
-            updateTrackLayerPath()
-            updatePointerLayerPath()
-        }
-    }
-    
-    private (set) var pointerAngle: CGFloat = CGFloat(-Double.pi) * 11 / 8
-    
-    func setPointerAngle(_ newPointerAngle: CGFloat, animated: Bool = false) {
-        CATransaction.begin()
-        CATransaction.setDisableActions(true)
-        
-        pointerLayer.transform = CATransform3DMakeRotation(newPointerAngle, 0, 0, 1)
-        
-        if animated {
-            let midAngleValue = (max(newPointerAngle, pointerAngle) - min(newPointerAngle, pointerAngle)) / 2
-                + min(newPointerAngle, pointerAngle)
-            let animation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
-            animation.values = [pointerAngle, midAngleValue, newPointerAngle]
-            animation.keyTimes = [0.0, 0.5, 1.0]
-            animation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
-            pointerLayer.add(animation, forKey: nil)
-        }
-        
-        CATransaction.commit()
-        
-        pointerAngle = newPointerAngle
-    }
-    
-    let trackLayer = CAShapeLayer()
-    let pointerLayer = CAShapeLayer()
-    
-    init() {
-        trackLayer.fillColor = UIColor.clear.cgColor
-        pointerLayer.fillColor = UIColor.clear.cgColor
-    }
-    
-    private func updateTrackLayerPath() {
-        let bounds = trackLayer.bounds
-        let center = CGPoint(x: bounds.midX, y: bounds.midY)
-        let offset = max(pointerLength, lineWidth  / 2)
-        let radius = min(bounds.width, bounds.height) / 2 - offset
-        
-        let ring = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle,
-                                endAngle: endAngle, clockwise: true)
-        trackLayer.path = ring.cgPath
-    }
-    
-    private func updatePointerLayerPath() {
-        let bounds = trackLayer.bounds
-        
-        let pointer = UIBezierPath()
-        pointer.move(to: CGPoint(x: bounds.width - CGFloat(pointerLength)
-            - CGFloat(lineWidth) / 2, y: bounds.midY))
-        pointer.addLine(to: CGPoint(x: bounds.width, y: bounds.midY))
-        pointerLayer.path = pointer.cgPath
-    }
-    
-    func updateBounds(_ bounds: CGRect) {
-        trackLayer.bounds = bounds
-        trackLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
-        updateTrackLayerPath()
-        
-        pointerLayer.bounds = trackLayer.bounds
-        pointerLayer.position = trackLayer.position
-        updatePointerLayerPath()
-    }
-}
-
-private class RotationGestureRecognizer: UIPanGestureRecognizer {
-    private(set) var touchAngle: CGFloat = 0
-    
-    override init(target: Any?, action: Selector?) {
-        super.init(target: target, action: action)
-        
-        maximumNumberOfTouches = 1
-        minimumNumberOfTouches = 1
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
-        super.touchesBegan(touches, with: event)
-        updateAngle(with: touches)
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
-        super.touchesMoved(touches, with: event)
-        updateAngle(with: touches)
-    }
-    
-    private func updateAngle(with touches: Set<UITouch>) {
-        guard
-            let touch = touches.first,
-            let view = view
-            else {
-                return
-        }
-        let touchPoint = touch.location(in: view)
-        touchAngle = angle(for: touchPoint, in: view)
-    }
-    
-    private func angle(for point: CGPoint, in view: UIView) -> CGFloat {
-        let centerOffset = CGPoint(x: point.x - view.bounds.midX, y: point.y - view.bounds.midY)
-        return atan2(centerOffset.y, centerOffset.x)
     }
 }
 
